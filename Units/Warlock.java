4d004be20c627086c4d9a5b1b50d2bdb231c6944
Mihail <Dante9041@mail.ru>
2023-04-07 18:51:58 +0300

    add README and Units 5

--- Units/Warlock.java
+++ Units/Warlock.java
@@ -1,6 +1,10 @@
 package OOP_java.Units;
 
+import java.util.ArrayList;
+
 public class Warlock extends BaseHero {
+    private ArrayList<Character> party;
+
 
     // Конструктор с параметрами
     public Warlock(String name) {
@@ -10,6 +14,27 @@ public class Warlock extends BaseHero {
     // Второй конструктор с возможностью задания всех характеристик героя
     public Warlock(int attack, int defense, int shots, String damage, int health, int speed, int delivery, int magic, String name) {
         super(attack, defense, shots, damage, health, speed, delivery, magic, name);
+    }
+     /**
+     * Колдун
+     * 
+     * @param name - Имя
+     */
+    public void step() {
+        if (!isDead()) {
+            setState("Stand");
+            Character target = null;
+            int lowestHealth = Integer.MAX_VALUE;
+            for (Character c : party) {
+                if (!c.isDead() && c.getHealth() < lowestHealth) {
+                    target = c;
+                    lowestHealth = c.getHealth();
+                }
+            }
+            if (target != null) {
+                heal(10); // Change this to adjust the amount healed
+            }
+        }
     }
 
 }
